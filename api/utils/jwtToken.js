6acdb9d91a84fb53552793be925fbcc70ffe6e29
Saq65 <saklainshaikh974@gmail.com>
2025-04-11 10:38:38 +0530

    k

--- api/utils/jwtToken.js
+++ api/utils/jwtToken.js
@@ -1,7 +1,6 @@
 export const sendToken = (user, statusCode, res) => {
     const token = user.getJWTToken();
 
-    //   OPTtion for cookie
     const options = {
         expires: new Date(Date.now() + process.env.
             COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
