9fc6748012b70017f51df075de6b1da28ecfaf83
Saq65 <saklainshaikh974@gmail.com>
2025-04-17 12:33:20 +0530

    some changes

--- api/App.js
+++ api/App.js
@@ -1,47 +1,41 @@
-
 import express from "express";
 import errorHandleMiddleware from "../api/middleware/error.js";
 import cookieParser from "cookie-parser";
 import userroutes from "./routes/UserRoutes.js";
 import cors from "cors";
 import inquiryRoutes from "./routes/InquiryRoutes.js";
-import http from "http"; // For creating the server
-import { Server } from "socket.io"; 
 
 const app = express();
 
-const server = http.createServer(app);
+// app.use(cors({
+//    origin: 'http://localhost:3000',  
+//    credentials: true,                
+// }));
 
-const io = new Server(server, {
-  cors: {
-    origin: [
-      "http://localhost:3000",
-      "https://apeksha-classes-orai.netlify.app"
-    ],
-    credentials: true,
-    methods: ["GET", "POST"]
-  }
-});
+app.use(cors({
+  origin: [
+   
+    "http://localhost:3000",
+    "https://apeksha-classes-orai.netlify.app",
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"]
+}));
 
-global.io = io;
 
-io.on("connection", (socket) => {
-  console.log("New client connected:", socket.id);
-});
+
+
+// Parse cookies and JSON
 app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
 
+// Routes
 app.use('/api/v1', userroutes);
 app.use("/api/v1", inquiryRoutes);
 
+// Error handler
 app.use(errorHandleMiddleware);
 
+
+
 export default app;
-
-// ✅ Export io to be used in other files
-export { io };
-
-// ✅ Run the server
-server.listen(8000, () => {
-  console.log("Server running on http://localhost:8000");
-});
