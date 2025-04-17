da7262fa18345b984e64035f08edf41ac63b66db
Saq65 <saklainshaikh974@gmail.com>
2025-04-17 13:44:12 +0530

    .

--- api/App.js
+++ api/App.js
@@ -12,13 +12,13 @@ const server = http.createServer(app);
 
 
 // Middleware
-// app.use(cors({
-//   origin: [
-//     "http://localhost:3000",
-//     "https://apeksha-classes-orai.netlify.app",
-//   ],
-//   credentials: true,
-// }));
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://apeksha-classes-orai.netlify.app",
+  ],
+  credentials: true,
+}));
 app.use(cookieParser());
 app.use(express.json({ limit: "10mb" }));
 
