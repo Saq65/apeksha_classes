e425c23899a89859f2ee7fa561448a443039cbe2
Saq65 <saklainshaikh974@gmail.com>
2025-04-17 13:56:07 +0530

    .

--- api/App.js
+++ api/App.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: ["https://apeksha-classes-orai.netlify.app", "http://localhost:3000"],
+  origin: "*",
   credentials: true
 }));
 app.use(express.json());
