0f04c71ae753a725f427f8caebe4b08a4155232d
Saq65 <saklainshaikh974@gmail.com>
2025-04-20 23:08:27 +0530

    link change

--- api/App.js
+++ api/App.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: ["https://apeksha-classes-orai.netlify.app", "http://localhost:3000"],
+  origin: ["https://apekshaclasses452.netlify.app", "http://localhost:3000"],
   credentials: true,
   allowedHeaders: ['Content-Type'],
 
