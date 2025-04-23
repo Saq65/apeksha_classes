5141a51f5f9571959322ae64990d6b2b83f77f0e
Saq65 <saklainshaikh974@gmail.com>
2025-04-23 15:55:30 +0530

    .

--- api/App.js
+++ api/App.js
@@ -1,6 +1,5 @@
 import express from "express";
 import http from "http";
-import { Server } from "socket.io";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -25,25 +24,8 @@ app.use(express.json());
 app.use("/api/v1", inquiryRoutes);
 app.use("/api/v1", userroutes);
 
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: ["https://apekshaclasses452.netlify.app", "http://localhost:3000"],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  }
-});
 
-app.set("io", io);
 
-io.on("connection", (socket) => {
-  console.log(" Socket connected:", socket.id);
-
-  socket.on("disconnect", () => {
-    console.log(" Socket disconnected:", socket.id);
-  });
-});
 
 
 export default app;
